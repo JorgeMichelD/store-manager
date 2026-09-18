284fa39ef843d810a325ecd015b23d3c65a98c35
Jorge Michel Daza <micheldazajorge@gmail.com>
2026-09-18 19:41:33 -0400

    Add barcode field to products

--- database.py
+++ database.py
@@ -21,31 +21,33 @@ CREATE TABLE IF NOT EXISTS productos(
     categoria TEXT NOT NULL,
     precio_compra REAL NOT NULL,
     precio_venta REAL NOT NULL,
-    cantidad INTEGER NOT NULL
+    cantidad INTEGER NOT NULL,
+    codigo_barras TEXT
 )
 """)
 
-#===============================================
+
+# =============================================
 # CREAR TABLA DE VENTAS
-#===============================================
+# =============================================
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS ventas(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-producto_id INTEGER NOT NULL,
-producto_nombre TEXT NOT NULL,
-cantidad INTEGER NOT NULL,
-precio_compra REAL NOT NULL,
-precio_venta REAL NOT NULL,
-total REAL NOT NULL,
-ganancia REAL NOT NULL,
-fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    producto_id INTEGER NOT NULL,
+    producto_nombre TEXT NOT NULL,
+    cantidad INTEGER NOT NULL,
+    precio_compra REAL NOT NULL,
+    precio_venta REAL NOT NULL,
+    total REAL NOT NULL,
+    ganancia REAL NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 """)
 
 
-#===============================================
+# =============================================
 # GUARDAR CAMBIOS
-#===============================================
+# =============================================
 
 conexion.commit()
